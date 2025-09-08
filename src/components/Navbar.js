d703a2707b981c9d8f3dbd38c99ee18b96cbf6c2
amadeu-murillo <104570746+amadeu-murillo@users.noreply.github.com>
2025-09-08 20:22:36 -0300

    Estrutura inicial

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import "./Navbar.css"
+import {Link,NavLink} from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <nav>
+     
+       <NavLink to="/" className={({isActive})=>(isActive ? "esta-ativo":"nao-ativo")}>Home</NavLink>
+       <NavLink to="/about">Sobre</NavLink>
+    </nav>
+  )
+}
+
+export default Navbar
