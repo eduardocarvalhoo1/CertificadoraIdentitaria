639f46964ca7ad3de245748e9e4490cf890e8ce8
luanvsalmeida <luanalmeida@alunos.utfpr.edu.br>
2025-10-02 11:22:56 -0300

    feat: swagger

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
-
+const swaggerUI = require("swagger-ui-express");
+const swaggerFile = require("../swagger-output.json");
 module.exports = () => {
 	const app = express();
 
 	app.set('port', process.env.PORT || config.get('server.port'));
 	app.use(bodyParser.json());
+	app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+	
 	return app;
 };
