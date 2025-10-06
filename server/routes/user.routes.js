58becc50e592cd550785cab524b3de03cb902ba1
luanvsalmeida <luanalmeida@alunos.utfpr.edu.br>
2025-10-06 01:04:57 -0300

    feat: authMiddleware

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const authMiddlware = require("../middlewares/auth.middleware");
 
 // POST /api/users
 router.post("/register", userController.createUser);
 
 router.post("/login", userController.login);
+
+router.post("/test", authMiddlware.verifyToken), async (req, res) => {
+    try {
+        res.status(200).json({ message: "deu baum" });
+    } catch (err) {
+        console.error("🔥 Full error:", err); // full object
+        console.error("🔥 Error keys:", Object.keys(err)); // see what properties exist
+        res.status(400).json({
+            error: err.message,
+            code: err.code || null,
+            stack: err.stack || null,
+            details: err.errorInfo || err.info || null
+        });
+  }
+};
 
 module.exports = router;
