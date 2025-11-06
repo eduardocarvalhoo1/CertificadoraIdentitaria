b78d7e1ec867f586f07c0b08b290905196b1179c
Eduardo Carvalho <carvalhooliveiraeduardo@gmail.com>
2025-11-05 23:28:45 -0300

    fix: Conserta erro de verificação de token

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -6,6 +6,12 @@ function verifyToken(req, res, next) {
     console.log(token);
     console.log(token);
     if (!token) return res.status(401).json({ error: 'Access denied' });
+
+    if (token.startsWith('Bearer ')) {
+        token = token.slice(7);
+    }
+
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.userId;
