2d50b2a2a7f6851496dae0ae7fa6440859c70f95
Eduardo Carvalho <carvalhooliveiraeduardo@gmail.com>
2025-11-18 23:28:20 -0300

    Revert "evert "Depois do pull"
    This reverts commit cde05bfe66ae2844493a3e1c7f402721ffd8a5aa, reversing
    changes made to ae8d0f22afaf37b9c8bfa79514d455e56baefb83.
    
    .git/COMMIT_EDITMSG [unix] (23:28 18/11/2025)
    1,1 Topo
    "~/Desktop/CertificadoraIdentitaria/CertificadoraIdentitaria/.git/COMMIT_EDITMSG"
    [unix] 16L, 498B
    
    This reverts commit cde05bfe66ae2844493a3e1c7f402721ffd8a5aa, reversing
    changes made to ae8d0f22afaf37b9c8bfa79514d455e56baefb83.

--- src/pages/Oficinas.js
+++ src/pages/Oficinas.js
@@ -146,30 +146,6 @@ export default function Oficinas() {
     }
   };
 
-  const handleSubscribe = async (oficina) => {
-    if (!token) {
-        setError("Sua sessão expirou. Faça login novamente.");
-        return;
-    }
-      try {
-        const response = await fetch(`http://localhost:8000/api/oficinas/${oficina.id}/inscrever`, {
-            method: 'POST',
-            headers: { 'Authorization': token },
-            body: {
-              userId: user.userId 
-            }
-        });
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.error || 'Falha ao inscrever na oficina.');
-        }
-        console.log(oficina);
-        setError(`Inscrição na oficina '${oficina.tema}' realizada com sucesso!`)
-      } catch (err) {
-        setError(err.message);
-      }
-  };
-
   const confirmDelete = () => {
     if (oficinaToDelete) {
       handleDelete(oficinaToDelete.id);
